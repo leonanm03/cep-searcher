5228d13d1295f5c73e7141634cd64ca92bb95a06
Leonan Martins <leonanm04@gmail.com>
2023-10-22 17:14:57 -0300

    chore: password min length

--- src/schemas/create-user-schema.ts
+++ src/schemas/create-user-schema.ts
@@ -3,9 +3,10 @@ import { object, string } from 'yup'
 export const createUserSchema = object({
     name: string().strict().required('Name is required'),
     email: string().strict().email().required('Email is required'),
-    password: string().strict().required('Password is required'),
+    password: string().min(8).strict().required('Password is required'),
     cpf: string()
+        .length(11)
         .strict()
         .required()
-        .matches(/^[0-9]{11}$/, 'must be a valid CPF')
+        .matches(/^[0-9]/, 'must be a valid CPF')
 })
